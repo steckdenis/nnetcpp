c1e040a90392b63709d21e181dec59f34109e70b
Denis Steckelmacher <steckdenis@yahoo.fr>
2015-08-04 21:31:09 +0200

    Fix the tanh activation function so that it matches its derivative
    Tanh layers now learn way faster.

--- tests/test_perceptron.cpp
+++ tests/test_perceptron.cpp
@@ -7,10 +7,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -124,7 +124,7 @@ void TestPerceptron::testActivation()
 
     CPPUNIT_ASSERT_MESSAGE(
         "Learning a cosinus function using a non-linear activation function",
-        checkLearning(net, input, output, 0.0001, 10000)
+        checkLearning(net, input, output, 0.001, 500)
     );
 
     delete net;
